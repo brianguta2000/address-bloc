6909921908f34e56b90bead1b7a11ff1a1fb6b8c
unknown <brianguta2000@yahoo.com>
2019-04-11 19:32:37 -0400

    redo assignment

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,54 +1,78 @@
 const inquirer = require('inquirer');
 
- module.exports = class MenuController {
-   constructor(){
-   	 this.mainMenuQuestions = [
-      {
-       type: "list",
-        name: "mainMenuChoice",
-        message: "Please choose from an option below: ",
-        choices: [
-          "Add new contact",
-          "Exit"
-        ]
-      }
-    ];
-    this.contacts = [];
-   }
- 
+module.exports = class MenuController{
+    constructor(){
+        this.mainMenuQuestions = [
+            {
+                type:'list',
+                name:'mainMenuChoice',
+                message:'Please choose from an option below: ',
+                choices:[
+                    'Add new Contact',
+                    'Today date is',
+                    'Exit',
+                ]
+            }
+        ];
+        this.contacts =  [];
+    }
 
- main(){
-     console.log(`Welcome to AddressBloc!`);
-     inquirer.prompt(this.mainMenuQuestions).then((response) => {
-       switch(response.mainMenuChoice){
-         case "Add new contact":
-           this.addContact();
-           break;
-         case "Exit":
-           this.exit();
-         default:
-           console.log("Invalid input");
-           this.main();
-       }
-     })
-     .catch((err) => {
+main(){
+   console.log('Welcome to AddressBloc!');
+   inquirer.prompt(this.mainMenuQuestions).then((response) => {
+     switch(response.mainMenuChoice){
+         case 'Add new Contact':
+          this.addContact();
+          break;
+
+         case "Today date is": 
+          this.getDate(); 
+          break;
+
+         case 'Exit':
+         this.exit();
+
+        default:
+        console.log('Invalid input');
+        this.main();
+     }
+   })
+   .catch((err) => {
        console.log(err);
-     });
-   }
+   });
+}
 
-   clear(){
-     console.log("\x1Bc");
-   }
-   addContact(){
-     this.clear();
-     console.log('addContact called');
-     this.main();
-   }
-    exit(){
-     console.log("Thanks for using AddressBloc!");
-     process.exit();
-   }
-     getContactCount(){
-     return this.contacts.length;
-   }
- }
+clear(){
+    console.log("\x1Bc");
+}
+
+getDate(){
+    this.clear();
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+    
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+    today = mm + '/' + dd + '/' + yyyy;
+    console.log(today);
+    this.main();
+}
+
+addContact(){
+    this.clear();
+    console.log('addContact called');
+    this.main();
+}
+
+exit(){
+    console.log('Thanks for using AddressBloc!');
+    process.exit();
+}
+}
